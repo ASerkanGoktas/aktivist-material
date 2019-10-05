64e20857958c90525111dddb15a55a5161efc2b4
serkan göktaş <aserkangoktas@gmail.com>
2019-10-05 14:28:17 +0300

    search bar üstte

--- app/src/navbar/navbar.component.js
+++ app/src/navbar/navbar.component.js
@@ -1,9 +1,12 @@
 import tpl from "./navbar.template.html"
 
 class NavbarController {
-    constructor($mdSidenav) {
-      
+    constructor($mdSidenav, $mdMedia) {
+        
+        this.media = $mdMedia;
+
         this.buildToggler = componentId => {
+            
             return () => {
                 $mdSidenav(componentId).toggle();
             }
@@ -11,13 +14,23 @@ class NavbarController {
 
         this.toggleLeft = this.buildToggler("left");
 
-        
+        this.isSearchOpen = false;
 
         
     }
+
+    isSmaller(brkpoint){
+        return this.media('max-width: ' + brkpoint);
+    }
+
+    toggleSearch(){
+        this.isSearchOpen = !this.isSearchOpen;
+
+        return this.isSearchOpen;
+    }
 }
 
-NavbarController.$inject = ["$mdSidenav"]
+NavbarController.$inject = ["$mdSidenav", "$mdMedia"]
 
 
 export default {
