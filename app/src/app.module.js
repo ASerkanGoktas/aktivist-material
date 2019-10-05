cbc508ef0b9e58ed0345553c45e987734ccf8464
serkan göktaş <aserkangoktas@gmail.com>
2019-10-05 15:20:16 +0300

    toolbarı uzattım

--- app/src/app.module.js
+++ app/src/app.module.js
@@ -4,7 +4,8 @@ import greetModule from './greet/greet.module';
 import navbarModule from "./navbar/navbar.module";
 import maincontentmodule from "./main-content/main-content.module";
 import detailsmodule from "./act-details/act-details.module";
+import ngaria from "angular-aria";
 import "../../node_modules/angular-material/angular-material.css"
 
 
-angular.module('aktivist', [greetModule, navbarModule, maincontentmodule, detailsmodule]);
+angular.module('aktivist', [greetModule, navbarModule, maincontentmodule, detailsmodule, ngaria]);
