c076a562ffab8b306be08ec23c094689af0cbbf1
serkan göktaş <aserkangoktas@gmail.com>
2019-10-14 00:30:52 +0300

    yutub çakması kapalı icon kısmı

--- app/src/container/contanier.component.js
+++ app/src/container/contanier.component.js
@@ -2,12 +2,17 @@ import tpl from "./container.template.html"
 
 
 class ContainerController{
-    constructor(SidenavService){
+    constructor(SidenavService, $mdMedia){
+        this.mdMedia = $mdMedia;
         this.sidenav = SidenavService;
+    }
+
+    isSmaller(brkpoint){
+        return this.mdMedia("max-width: " + brkpoint);
     }
 }
 
-ContainerController.$inject = ["SidenavService"];
+ContainerController.$inject = ["SidenavService", "$mdMedia"];
 export default {
     template: tpl,
     controller: ContainerController
