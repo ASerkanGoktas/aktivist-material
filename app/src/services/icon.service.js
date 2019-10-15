7df696dba3cfbb678ada1f9eb806c4e4e3878137
serkan göktaş <aserkangoktas@gmail.com>
2019-10-15 15:44:36 +0300

    added dist to gitignore

---
+++ app/src/services/icon.service.js
@@ -0,0 +1,19 @@
+import concert from "../assets/icons/jazz-saxophone.svg";
+import museum from "../assets/icons/bank.svg";
+import theater from "../assets/icons/theater-masks.svg";
+import movie from "../assets/icons/film-roll.svg";
+import hamburger from "../assets/icons/icon_menu.svg";
+
+class IconService{
+    constructor(){
+        this.concert = concert;
+        this.museum = museum;
+        this.theater = theater;
+        this.movie = movie;
+        this.hamburger = hamburger;
+
+        
+    }
+}
+
+export default IconService;
