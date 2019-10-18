4b713391e9e68287c3741d871a140ac41d086e71
serkan göktaş <aserkangoktas@gmail.com>
2019-10-18 23:14:32 +0300

    reverted changes

--- app/src/navbar/navbar.module.js
+++ app/src/navbar/navbar.module.js
@@ -2,6 +2,6 @@ import angular from 'angular';
 import NavbarComponent from './navbar.component';
 import ngmaterial from "angular-material"
 
-let mod = angular.module('navbar', [ngmaterial, "angularScreenfull"]).component('navbar', NavbarComponent);
+let mod = angular.module('navbar', [ngmaterial]).component('navbar', NavbarComponent);
 
 export default mod.name;
