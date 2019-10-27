c0d58d2994e32d0f44406412a84809a5a7e72862
serkan göktaş <aserkangoktas@gmail.com>
2019-10-28 00:08:44 +0300

    navbar disappears when scrollup

--- app/src/container/contanier.component.js
+++ app/src/container/contanier.component.js
@@ -1,19 +1,48 @@
 import tpl from "./container.template.html"
+import "angular";
 
-
-class ContainerController{
-    constructor(SidenavService, IconService, $mdMedia){
+class ContainerController {
+    constructor(SidenavService, IconService, $mdMedia, $document) {
+        this.doc = $document;
+        this.position = 0;
         this.icon = IconService;
         this.mdMedia = $mdMedia;
         this.sidenav = SidenavService;
+        this.isNav = true;
+
+        this.doc.find("md-content")[1].onscroll = e => {
+            let currentPos = e.target.scrollTop;
+            console.log(this.isNav);
+
+            if (currentPos > this.position) {
+                //scrolling down
+
+                this.isNav = true;
+                let a = angular.element(this.doc.find('navbar')[0]);
+                console.log(a);
+                a.removeClass("hidden");
+                this.position = currentPos;
+            }else {
+                this.isNav = false;
+                let a = angular.element(this.doc.find('navbar')[0]);
+                console.log(a);
+                a.addClass("hidden");
+                this.position = currentPos;
+            }   
+
+            
+
+
+        }
     }
 
-    isSmaller(brkpoint){
+    isSmaller(brkpoint) {
         return this.mdMedia("max-width: " + brkpoint);
     }
+
 }
 
-ContainerController.$inject = ["SidenavService", "IconService","$mdMedia"];
+ContainerController.$inject = ["SidenavService", "IconService", "$mdMedia", "$document"];
 export default {
     template: tpl,
     controller: ContainerController
--- app/src/homepage/homepage.component.js
+++ app/src/homepage/homepage.component.js
@@ -2,7 +2,7 @@ import tpl from "./homepage.template.html"
 
 class HomepageController{
     constructor(){
-        this.acts = [1, 2, 3, 4, 5]
+        this.acts = [1, 2, 3, 4, 5, 6, 7, 8]
     }
 }
 
