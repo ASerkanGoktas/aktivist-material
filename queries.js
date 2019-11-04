a6d6aeaedbb603c8077e74fe8b43c70da33e87e1
serkan göktaş <aserkangoktas@gmail.com>
2019-11-05 00:03:52 +0300

    forgot to add queries.js

---
+++ queries.js
@@ -0,0 +1,24 @@
+const Pool = require('pg').Pool
+
+const pool = new Pool({
+    user: "kqddeaplbhelix",
+    host: "ec2-46-137-113-157.eu-west-1.compute.amazonaws.com",
+    database: "d9bblfnn09vkg7",
+    port: "5432",
+    password: "8257debded76d2a2b1cdf810cfb28939b450e88616b9778ee18b70308922501a"
+})
+
+const getAll = (request, response) => {
+    pool.query('SELECT * FROM etkinlik', (error, results) => {
+        if(error){
+            console.log("error");
+            console.log(error);
+        }else{
+            response.status(200).json(results.rows);
+        }
+    });
+};
+
+module.exports = {
+    getAll
+};
