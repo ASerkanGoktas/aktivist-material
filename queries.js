f85fa32195138f5e113330d689dcf826a4630cfb
serkan göktaş <aserkangoktas@gmail.com>
2020-01-19 01:17:13 +0300

    pff

--- queries.js
+++ queries.js
@@ -1,25 +1,24 @@
 const Pool = require('pg').Pool
 
-const pool = new Pool({
+const pool = new Pool(/* {
     user: "kqddeaplbhelix",
     host: "ec2-46-137-113-157.eu-west-1.compute.amazonaws.com",
     database: "d9bblfnn09vkg7",
     port: "5432",
     password: "8257debded76d2a2b1cdf810cfb28939b450e88616b9778ee18b70308922501a"
-}/* {
+} */{
     user: "postgres",
     host: "localhost",
     database: "aktivistlocal",
     port: "5432",
     password: "279157"
-} */)
+})
 
 const getAll = (request, response) => {
-    pool.query('SELECT * FROM etkinlik JOIN instances ON (etkinlik.etkinlik_id = instances.etkinlik_id)', (error, results) => {
-        console.log('nabeeer be kankaaa');
+    pool.query('SELECT * FROM event JOIN instance ON (event.event_id = instance)', (error, results) => {
+        
         if(error){
-            console.log("error");
-            console.log(error);
+
         }else{
             response.status(200).json(results.rows);
         }
@@ -27,25 +26,24 @@ const getAll = (request, response) => {
 };
 
 const get_activity = (request, response) => {
-    pool.query('SELECT * FROM etkinlik JOIN instances ON' + 
-    '(etkinlik.etkinlik_id = instances.etkinlik_id) WHERE instance_id = ' + request.params.id, (error, results) => {
-        console.log('off manyak');
+    pool.query('SELECT *  FROM event JOIN instance ON' + 
+    '(event.event_id = instance.event) WHERE instance_id = ' + request.params.id, (error, results) => {
+        
         if(error){
-            console.log("error");
             console.log(error);
         }
         else{
+            console.log(results.rows[0]);
             response.status(200).json(results.rows[0]);
         }
     });
 }
 
 const get_prices_of_act = (request, response) => {
-    pool.query('SELECT * FROM fiyat WHERE instance_id = ' + request.params.id, (error, results) => {
-        console.log('fiyat caigirldi');
+    pool.query('SELECT * FROM price WHERE instance = ' + request.params.id, (error, results) => {
+        
         if(error){
-            console.log("error");
-            console.log(error);
+            
         }
         else{
             
@@ -57,7 +55,7 @@ const get_prices_of_act = (request, response) => {
 const filter_activities = (request, response) => {
 
     const NONE = "NONE";
-    qry = "SELECT * FROM instances JOIN etkinlik ON (etkinlik.etkinlik_id = instances.etkinlik_id) WHERE";
+    qry = "SELECT * FROM instance JOIN event ON (event.event_id = instance.event) WHERE";
     let dateFirst, dateSecond, city;
     dateFirst = request.params.datefirst;
     dateSecond = request.params.datesecond;
@@ -79,13 +77,11 @@ const filter_activities = (request, response) => {
     qry = qry.substring(0, qry.length - 3);
     
     pool.query(qry, (error, results) => {
-        console.log("query:");
-        console.log(qry);
+        
         
         
         if(error){
-            console.log("error");
-            console.log(error);
+            
         }
         else{
             
@@ -96,17 +92,17 @@ const filter_activities = (request, response) => {
 
 
 const liveSearch = (request, response) => {
-    qry = "SELECT * FROM instances JOIN etkinlik ON (etkinlik.etkinlik_id = instances.etkinlik_id) WHERE";
-    qry = qry.concat(" LOWER(etkinlik.name) LIKE LOWER('", request.params.actname, "%')");
+    qry = "SELECT * FROM instance JOIN event ON (event.event_id = instance.event) WHERE";
+    qry = qry.concat(" LOWER(event.name) LIKE LOWER('", request.params.actname, "%')");
+    qry = qry.concat(" LIMIT 5");
 
     console.log(qry);
     pool.query(qry, (error, results) => {
         if(error){
-            console.log("error");
-            console.log(error);
+            
         }else{
             response.status(200).json(results.rows);
-            console.log(results.rows);
+            
         }
     });
 }
