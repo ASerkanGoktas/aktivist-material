d9e1e12633992300e662899ae19f7ff1ace668f6
aserkangoktas <aserkangoktas@gmail.com>
2020-03-27 03:13:38 +0300

    added slide to horizontal scroll

--- app/src/homepage/homepage.component.js
+++ app/src/homepage/homepage.component.js
@@ -1,14 +1,96 @@
 import tpl from "./homepage.template.html"
 
 class HomepageController{
-    constructor(EtkinlikService, $rootScope){
+    constructor(EtkinlikService, FilterService, $rootScope, $document){
         this.acts = []
         this.et = EtkinlikService;
         this.et.get_todays_movies("İstanbul").then(response => {
             this.todays_movies = response.data;
         });
-
+        this.filtersrv = FilterService;
+        this.today = this.get_today();
+        this.document = $document;
+        
         $rootScope.$broadcast("clearSearch", null);
+    }
+
+    get_today(){
+        var today = new Date();
+    
+        today= `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate() - 15}`;
+        return today;
+    }
+
+    trim_name(name) {
+        name = name.replace(/=/, "'");
+        return ((name.length < 60) ? name : name.slice(0, 56) + "...")
+    }
+
+    get_subtitle(act) {
+        var day = this.get_gunSayi(act.date);
+        var month = this.get_ay(act.date);
+
+        var day_name = this.get_gun(act.date);
+
+        return `${day} ${month} ${day_name}`;
+    }
+
+    get_gun(datestr) {
+        var temp_date = new Date(datestr);
+
+        return this.gunler[temp_date.getDay()];
+    }
+
+    get_ay(datestr) {
+        var temp_date = new Date(datestr);
+
+        return this.aylar[temp_date.getMonth()];
+    }
+
+    get_saat(timestr) {
+        var temp_date = new Date(timestr);
+        var saat = temp_date.getHours();
+        var dakika = temp_date.getMinutes();
+        if (dakika == 0) {
+            dakika = "00";
+        }
+
+        //GMT +3
+        saat = saat - 3;
+
+        return saat + ":" + dakika;
+    }
+
+    get_gunSayi(datestr) {
+        var temp_date = new Date(datestr);
+
+        return temp_date.getDate();
+    }
+
+    scroll_right(id){
+        var list = this.document[0].getElementById(id)
+
+        var scrollAmount = 0
+        var slideTimer = setInterval(function(){
+            list.scrollLeft += 60;
+            scrollAmount += 10;
+            if(scrollAmount >= 100){
+                window.clearInterval(slideTimer);
+            }
+        }, 25);
+    }
+
+    scroll_left(id){
+        var list = this.document[0].getElementById(id)
+
+        var scrollAmount = 0
+        var slideTimer = setInterval(function(){
+            list.scrollLeft -= 60;
+            scrollAmount += 10;
+            if(scrollAmount >= 100){
+                window.clearInterval(slideTimer);
+            }
+        }, 25);
     }
 }
 
@@ -17,4 +99,4 @@ export default {
     controller: HomepageController
 }
 
-HomepageController.$inject = ["EtkinlikService", "$rootScope"];
+HomepageController.$inject = ["EtkinlikService", "FilterService", "$rootScope", "$document"];
